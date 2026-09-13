e69515e7cf98131b238767ddc44fee61678996e3
Muslim <eng.muslim03@gmail.com>
2026-09-14 01:27:11 +0300

    Refactor piso module inputs and output behavior
    Refactor piso module to improve clarity and functionality.

--- rtl/piso.v
+++ rtl/piso.v
@@ -1,25 +1,21 @@
 module piso #(
     parameter DATA_WIDTH = 4
 )(
-    input clk,rst,load,en,
+    input clk,
+    input load, //allow piso to load new bus of bits inside it
+    input out_en, //allow piso to out the LSB
     input [DATA_WIDTH-1:0]in,
     output reg out
 );
 
-reg [DATA_WIDTH-1:0]q;
+reg [DATA_WIDTH-1:0]q; //inside register
 
 always @(posedge clk) begin
-    if(rst)begin
-        q<=0;
-        out<=0;
-    end
-    else if (en) begin
-        if(load)begin
+    if(load)begin
         q<=in;
-        end
-        else begin
-        {q[DATA_WIDTH-2:0],out}<=q;
-        end
+    end
+    else if (out_en) begin
+        {q[DATA_WIDTH-2:0],out}<=q; //shift right by 1 bit
     end
 end
 
