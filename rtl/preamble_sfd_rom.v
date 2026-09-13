17d6db7056ec0efee0d27cfc7314e3cfc0e81667
Muslim <eng.muslim03@gmail.com>
2026-09-14 01:38:56 +0300

    updated rom
    Updated preamble_sfd_rom module to include clock input and change data output to reg type.

--- rtl/preamble_sfd_rom.v
+++ rtl/preamble_sfd_rom.v
@@ -2,8 +2,9 @@ module preamble_sfd_rom #(
 parameter ADDR_WIDTH = 4,
 parameter DATA_WIDTH = 4
 )(
+    input clk,
     input [ADDR_WIDTH-1:0] addr,
-    output [DATA_WIDTH-1:0] data
+    output reg [DATA_WIDTH-1:0] data
 );
 
     reg [DATA_WIDTH-1:0] rom [0:(2**ADDR_WIDTH)-1];
@@ -12,6 +13,8 @@ parameter DATA_WIDTH = 4
         $readmemb("preambleSFD.txt",rom);
     end
 
-    assign data = rom[addr];
+    always @(posedge clk) begin
+        data <= rom[addr];
+    end
 
 endmodule
