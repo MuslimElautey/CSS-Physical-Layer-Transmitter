132390265bd05f9a85692165b3e73a114c30eb4c
Muslim <eng.muslim03@gmail.com>
2026-09-14 01:25:59 +0300

    Refactor mux2x1 module to use parameter for data width

--- rtl/mux2x1.v
+++ rtl/mux2x1.v
@@ -1,7 +1,9 @@
-module mux2x1(
-    input [3:0] in0,in1,
-    input s,
-    output [3:0] out
+module mux2x1 #(
+    parameter DATA_WIDTH=4
+)(
+    input [DATA_WIDTH-1:0] in0,in1,
+    input sel,
+    output [DATA_WIDTH-1:0] out
 );
-    assign out =(s)? in1:in0;
+    assign out =(sel)? in1:in0;
 endmodule
