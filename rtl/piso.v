c86204ef08e9c8bda0945ce29e492561f02e0fa0
Muslim <eng.muslim03@gmail.com>
2026-09-14 18:16:12 +0300

    Update piso.v

--- rtl/piso.v
+++ rtl/piso.v
@@ -5,7 +5,7 @@ module piso #(
     input load, //allow piso to load new bus of bits inside it
     input out_en, //allow piso to out the LSB
     input [DATA_WIDTH-1:0]in,
-    output reg out
+    output reg out,valid
 );
 
 reg [DATA_WIDTH-1:0]q; //inside register
@@ -13,9 +13,14 @@ reg [DATA_WIDTH-1:0]q; //inside register
 always @(posedge clk) begin
     if(load)begin
         q<=in;
+        valid<=0;
     end
     else if (out_en) begin
+        valid<=1;
         {q[DATA_WIDTH-2:0],out}<=q; //shift right by 1 bit
+    end
+    else begin
+        valid<=0;
     end
 end
 
